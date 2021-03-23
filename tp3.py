4f52a156661a4d81e52218cf436a489cff10ab0c
Cobrette <78154581+Cobrette@users.noreply.github.com>
2021-03-23 16:31:41 +0100

    Update tp3.py

--- tp3.py
+++ tp3.py
@@ -84,3 +84,17 @@ def expo_dg(x,k):
         n=n*2
         x=x*x
     return l
+#exo 3: Nombres de Fibonacci
+#question 1 :
+def fibo(n):
+    f0 = 0
+    f1 = 1
+    fn = -1
+    if (n == 0):
+        fn = f0
+    if (n == 1):
+        fn = f1
+    for k in range(2,n+1):
+        fn = f1 + f0
+        f0,f1 = f1,fn
+    return fn
