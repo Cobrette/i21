eca99c606d123fd9fdaa86af7093c163d8ca00cd
Cobrette <78154581+Cobrette@users.noreply.github.com>
2021-04-27 18:53:43 +0200

    Add files via upload

---
+++ tp7.py
@@ -0,0 +1,18 @@
+def pile_affiche(P):
+    l=0
+    P2=pile_copie(P)
+    while not pile_vide(P2):
+        el=len(str(depiler(P2)))
+        if l<el
+        l=el
+    separatrice=""*(l+4)
+    p3=pile_copie(P)
+    print(separatrice)
+    while not pile_vide(P3):
+        element=str(depiler(P3))
+        ligne="l"+(l-lan(element)*""+element+"l"
+        print(ligne)
+        print(separatrice)
+        
+def file_affiche(F):
+    
